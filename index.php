e745fd9d8cc91c201b9af18a8558e580cf74102c
Muhammad Akmal Firmansyah <akmalfrmnsyh774@gmail.com>
2025-07-16 23:17:44 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus doloremque debitis veritatis non dignissimos atque nemo esse beatae sed natus earum sunt numquam tempora officia assumenda possimus, recusandae exercitationem provident.</p>
+</body>
+</html>
